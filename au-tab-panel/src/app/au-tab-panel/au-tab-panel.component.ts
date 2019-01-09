1ff087785493296a77d2071b93facba7a5e87f5a
Gabor Schmied <gabor.schmied@cognex.com>
2019-01-09 17:21:46 +0100

    initial implementation of the tab panel and the tabs

--- au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
+++ au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { AuTabComponent } from 'app/au-tab/au-tab.component';
 
 @Component({
   selector: 'au-tab-panel',
   templateUrl: './au-tab-panel.component.html',
   styleUrls: ['../tab-panel.component.scss']
 })
-export class AuTabPanelComponent implements OnInit {
+export class AuTabPanelComponent implements OnInit, AfterContentInit {
+
+  @ContentChildren(AuTabComponent)
+  tabs: QueryList<AuTabComponent>;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit() {
+    const selectedTab = this.tabs.find(x => x.selected);
+    if (!selectedTab) {
+      this.tabs.first.selected = true;
+    }
+  }
+
+  selectTab(tab: AuTabComponent): void {
+    this.tabs.forEach(x => x.selected = false);
+    tab.selected = true;
+  }
 }
